f2751f2ac3a910a2fd08806906ad22b73937d473
Tanaka Yoshihito <gooogle.tanaka@gmail.com>
2026-08-31 15:03:36 +0900

    設定画面追加

--- src/components/preferences/PreferencesDialog.tsx
+++ src/components/preferences/PreferencesDialog.tsx
@@ -1,64 +1,53 @@
-import { useState } from "react";
 import { Settings, Zap } from "lucide-react";
 
-import { useUiStore } from "@/store/ui-store";
 import { Dialog, DialogContent } from "../ui/dialog";
-import { Sidebar, SidebarContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider } from "../ui/sidebar";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
+
+import { useUiStore } from "@/store/ui-store";
 import { GeneralPane } from "./GeneralPane";
 import { AdvancedPane } from "./AdvancedPane";
 
-type PreferencePane = 'general' | 'advanced';
 
-const navigationItems = [
+const paneList = [
   {
     id: 'general' as const,
     label: '基本',
     icon: Settings,
+    node: GeneralPane
   },
   {
     id: 'advanced' as const,
     label: '高度な設定',
     icon: Zap,
+    node: AdvancedPane
   },
 ] as const;
 
 export function PreferencesDialog() {
-  const [activePane, setActivePane] = useState<PreferencePane>('general')
   const showPreferencesDialog = useUiStore(state => state.showPreferencesDialog);
   const setShowPreferencesDialog = useUiStore(state => state.setShowPreferencesDialog);
 
   return (
     <Dialog open={showPreferencesDialog} onOpenChange={setShowPreferencesDialog}>
       <DialogContent className="max-w-[90%] max-h-[90%]">
-        <SidebarProvider >
-          <Sidebar className="h-full">
-            <SidebarContent>
-              <SidebarMenu>
-                {navigationItems.map(item => (
-                  <SidebarMenuItem key={item.id}>
-                    <SidebarMenuButton
-                      isActive={activePane === item.id}
-                    >
-                      <button
-                        onClick={() => setActivePane(item.id)}
-                        className="flex items-center"
-                      >
-                        <item.icon />
-                        <span className="pl-1">{item.label}</span>
-                      </button>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))}
-              </SidebarMenu>
-            </SidebarContent>
-          </Sidebar>
-          <main>
-            <div>
-              {activePane === 'general' && <GeneralPane />}
-              {activePane === 'advanced' && <AdvancedPane />}
-            </div>
-          </main>
-        </SidebarProvider>
+        <div className="flex">
+          <Tabs orientation='vertical'>
+            <TabsList>
+              {paneList.map(item => (
+                <TabsTrigger key={item.id} value={item.id}
+                >
+                  <item.icon />
+                  {item.label}
+                </TabsTrigger>
+              ))}
+            </TabsList>
+            {paneList.map(item => (
+              <TabsContent key={item.id} value={item.id}>
+                <item.node />
+              </TabsContent>
+            ))}
+          </Tabs>
+        </div>
       </DialogContent>
     </Dialog >
   )
