3c88460cb2f94fdac76314a3eaf2eb0f219e31f0
Tanaka Yoshihito <gooogle.tanaka@gmail.com>
2026-09-12 21:50:35 +0900

    ファイルアイコン変更

--- src/components/file-list/FileListRow.tsx
+++ src/components/file-list/FileListRow.tsx
@@ -29,18 +29,18 @@ function Icon({
     icon = <WhiteMediumSquareIcon height="100%" />;
   } else if (dirEntry.is_symlink && dirEntry.is_dir) {
     icon = (
-      <div className="relative">
+      <div style={{ height: "1lh" }} className="relative">
         <FileFolderIcon height="100%" />
-        <div className="absolute right-0 bottom-0 w-[60%]">
+        <div className="absolute right-0 bottom-0 w-[50%]">
           <UpRightArrowIcon />
         </div>
       </div>
     );
   } else if (dirEntry.is_symlink) {
     icon = (
-      <div className="relative">
+      <div style={{ height: "1lh" }} className="relative">
         <FileExtIcon dirEntry={dirEntry} />
-        <div className="absolute right-0 bottom-0 w-[60%]">
+        <div className="absolute right-0 bottom-0 w-[50%]">
           <UpRightArrowIcon />
         </div>
       </div>
