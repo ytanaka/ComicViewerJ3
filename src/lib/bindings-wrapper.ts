47ef3c317d11715fd319ef376f83adcc9c4f5b2a
Tanaka Yoshihito <gooogle.tanaka@gmail.com>
2026-09-14 22:35:55 +0900

    コメント更新

--- src/lib/bindings-wrapper.ts
+++ src/lib/bindings-wrapper.ts
@@ -113,6 +113,7 @@ export function mkAppPreferencesDefault(): AppPreferences {
     debug_filename_search_sleep_ms: 0,
     filename_sort_strength: 'Identical',
     filename_cmp: { type: 'Icu' },
+    thumbnail_expiration_hours: 0,
   };
 }
 
