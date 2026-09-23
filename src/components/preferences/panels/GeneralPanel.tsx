c3368c3b58c41f46650db4156e41324e4b075a8c
Tanaka Yoshihito <gooogle.tanaka@gmail.com>
2026-09-23 15:30:34 +0900

    フォント設定画面追加

--- src/components/preferences/panels/GeneralPanel.tsx
+++ src/components/preferences/panels/GeneralPanel.tsx
@@ -1,9 +1,9 @@
 import { Field, FieldDescription, FieldGroup, FieldLabel, FieldLegend, FieldSet } from '@/components/ui/field';
-import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 
 import { InputGroup, InputGroupInput } from '@/components/ui/input-group';
 import { Separator } from '@/components/ui/separator';
-import { DEFAULT_FONT_FAMILY, FONT_FAMILY_LIST, MAX_TAB_NUM_LIMIT, useUiStore } from '@/store/ui-store';
+import { FONT_FAMILY_LIST, MAX_TAB_NUM_LIMIT, useUiStore } from '@/store/ui-store';
+import { NativeSelect, NativeSelectOption } from '@/components/ui/native-select';
 
 export function GeneralPanel() {
   const maxTabNum = useUiStore(state => state.maxTabNum);
@@ -33,21 +33,13 @@ export function GeneralPanel() {
         <Separator />
         <Field>
           <FieldLabel>フォント</FieldLabel>
-          <Select value={fontFamily} onValueChange={(v) => setField('fontFamily', v ?? DEFAULT_FONT_FAMILY)}>
-            <SelectTrigger className="w-full max-w-48">
-              <SelectValue>{fontFamily}</SelectValue>
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                {/* <SelectItem key='' value={undefined}>デフォルト</SelectItem> */}
-                {FONT_FAMILY_LIST.map(item => (
-                  <SelectItem key={item} value={item}>
-                    {item}
-                  </SelectItem>
-                ))}
-              </SelectGroup>
-            </SelectContent>
-          </Select>
+          <NativeSelect className='max-w-40' value={fontFamily} onChange={(e) => { setField('fontFamily', e.target.value) }}>
+            {FONT_FAMILY_LIST.map(item => (
+              <NativeSelectOption key={item} value={item}>
+                {item}
+              </NativeSelectOption>
+            ))}
+          </NativeSelect>
           <div className='flex items-center'>
             <InputGroup className="max-w-40">
               <InputGroupInput
